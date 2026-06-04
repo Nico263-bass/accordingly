c4d61705176871b1efe4a5bc30968189a03873d8
Nico263-bass <Nicobomolo263@gmail.com>
2026-06-04 18:15:44 +0100

    Correction du bug compteur

---
+++ restaurantMenu./Algorithme_de_Fischer_Yates.js
@@ -0,0 +1,11 @@
+// Algorithme de fisher Yates
+const tab=['a','b','c','d','e'];
+function fishY(tabArray){
+   for (let i =tabArray.length-1; i>0; i--) {
+        const j=Math.floor(Math.random()*(1+i));
+        [tabArray[i],tabArray[j]]=[tabArray[j],tabArray[i]]
+   }
+   return tabArray;
+        
+}
+console.log(fishY(tab));
--- restaurantMenu./restaurant_menu.js
+++ restaurantMenu./restaurant_menu.js
@@ -1,7 +1,22 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+/* const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
  const mainCourseMenuHTML=mainCourseMenu.map((el,index)=>`<p>${el}:son index est ${index+1}</p>`).join('');
     document.getElementById('maincourseMenuItems').innerHTML=mainCourseMenuHTML;
+for (let index = 0; index < array.length; index++) {
+       const element = array[index];
+       
+} */
+
+const obj={
+       nom:'nomObjet',
+};
+try {
+       const prop=obj.age;
+       console.log(`la valeur de la la propriété est: ${prop}`);
+} catch (error) {
+       console.error(`Une erreur a étée rencontrée lors de la lecture de la proprité: ${error.message}`);
+}
+console.log('le code continue à s\'exécuter malgré l\'erreur rencontrée');
