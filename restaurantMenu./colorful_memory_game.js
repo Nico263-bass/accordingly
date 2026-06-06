91142629489c31549c2fdef67f58223c1f17dd88
Nico263-bass <Nicobomolo263@gmail.com>
2026-06-06 22:56:41 +0100

    Fin de l'implémentation de l'algorithme de fisher-Yates ainsi que de la création du compte à rebours,la prochaine étape consiste à implémenter les animations css pour retourner les cartes une à une

--- restaurantMenu./colorful_memory_game.js
+++ restaurantMenu./colorful_memory_game.js
@@ -1,136 +1,61 @@
-const list=["&#128512;","&#128513;","&#128514;","&#128590;","&#128122;","&#128663;","&#128512;","&#128513;","&#128514;","&#128590;","&#128122;","&#128663;"];
+const list=["&#128512;","&#128513;","&#128514;","&#128590;","&#128122;"
+    ,"&#128663;","&#128512;","&#128513;","&#128514;","&#128590;",
+    "&#128122;","&#128663;"];
 /************************************************ */
 const divMain=document.querySelector('.main');
-      list.forEach(element=>{
-            const divImg=document.createElement('div');
-                  divImg.classList.add('img');
-                  divMain.appendChild(divImg);
-            const spanSmile= document.createElement('span');
-                  spanSmile.classList.add('smile');    
-                  divImg.appendChild(spanSmile);
-                  spanSmile.innerHTML=element;
+list.forEach(element=>{
+const divImg=document.createElement('div');
+      divImg.classList.add('img');
+      divMain.appendChild(divImg);
+const spanSmile= document.createElement('span');
+      spanSmile.classList.add('smile');    
+      divImg.appendChild(spanSmile);
+      spanSmile.innerHTML=element;
 })
 //Algo de Fisher-Yates pour le tirage aléatoire des cartes
 function fishY(tabArray){
-    for (let i =tabArray.length-1; i>0; i--) {
-            const j=Math.floor(Math.random()*(1+i));
-            [tabArray[i],tabArray[j]]=[tabArray[j],tabArray[i]];
-    }
-    return tabArray;
+for (let i =tabArray.length-1; i>0; i--) {
+        const j=Math.floor(Math.random()*(1+i));
+        [tabArray[i],tabArray[j]]=[tabArray[j],tabArray[i]];
+}
+return tabArray;
 }
 /**********Application de l'Algo de fisher Yates **************/
-// const btnStart=document.querySelector('#btnStart');
-const btnStart=document.querySelector('#btnStart');
-               btnStart.addEventListener('click',()=>{
-               const newList=fishY([...list]);
-               const spanSmile= document.querySelectorAll('.smile');
-               spanSmile.forEach((element,index)=>{
-               element.innerHTML=newList[index];
-       })    
- });
- const spanTime=document.querySelector('#time');    
-       time.textContent='';
-       btnStart.addEventListener('click',()=>{
-         let    time=20;
-         const  compteur=setInterval(()=>{ 
-                time--;
-                spanTime.textContent=time;
-                if (time===0){
-                    clearInterval(compteur);
-                    alert('Temps épuisé!');
-                    spanTime.textContent='';
-                }
-            },1000)
-       })
-    
-           
+// Function exécutant le compteur et l'algorithme de Fishr-Yates
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Fonction Asynchrone pour empếcher que le compteur à nouveau pendant qu'il est déjà en marhce 
-async function startGame(){ 
-    try {
-        const messageSioui = await new Promise((resolve, reject) => {
-            resolve(() => {
-                let time = 60;
-                const timing = setInterval(() => {
-                    time--;
-                    let chrono = document.querySelector('#time');
-                    chrono.textContent = `${time}s`;
-                    if (time === 0) {
-                        clearInterval(timing);
-                        alert('Fin de la partie');
-                        chrono.textContent = '60s';
-                    }
-                    const btnEnd = document.querySelector('#btnEnd');
-                    btnEnd.addEventListener('click', () => {
-                        clearInterval(timing);
-                        chrono.textContent = '60s';
-                        btnStart.removeEventListener('click');
-                    });
-                }, 1000);
-            });
-            reject(`Une erreur s'est produite!`);
-          }
-    )}catch (messageSinon) {
-        console.log(`échec de l'opération ${messageSinon}`);
-    }
-/****************Fonction chrono du jeu et start game****************
-
-let compteurEncours=false;
-    btnStart.addEventListener('click',async () => {
-        if (compteurEncours){
-            return
-        }
-        compteurEncours=true;
-        await startGame();
-        compteurEncours=false;
+function compteurAndFy(){
+return new Promise ((resolve)=>{
+                    
+const    newList=fishY([...list]);
+const    spanSmile= document.querySelectorAll('.smile');
+         spanSmile.forEach((element,index)=>{
+         element.innerHTML=newList[index];})    
+const    spanTime=document.querySelector('#time');    
+         spanTime.textContent='';
+let      time=10;
+const    compteur=setInterval(()=>{
+         time--;
+         spanTime.textContent=time;
+         if (time===0){
+            clearInterval(compteur);
+            spanTime.textContent='Temps épuisé!'; 
+            resolve(()=>{compteurEncours=false;});  
+        } },1000); 
     })
-}*/
-
+}
+const btnStart=document.querySelector('#btnStart');
+let compteurEncours=false; 
+       btnStart.addEventListener('click',async ()=>{
+   try {
+           if(compteurEncours){
+               return
+           }
+           else{
+                 compteurEncours=true;
+                 const attente=await compteurAndFy();
+                 attente();
+       }
+   } catch (error) {
+       console.error(`Une erreur s'est produite ${error}`)
+   }
+})
